6a052e506ed1de3a1b069d66dab639be70b7c10d
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-03 20:07:51 -0600

    Added utilities.go file and created writeJSON func that returns an error.

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"backend/models"
 	"errors"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -17,6 +19,19 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.logger.Println("id is", id)
+
+	movie := models.Movie{
+		ID:          id,
+		Title:       "Some Movie",
+		Description: "Some Description",
+		Year:        2021,
+		ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
+		Runtime:     100,
+		Rating:      5,
+		MPAARating:  "PG-13",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
 }
 
 func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {}
