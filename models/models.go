38adb5c78a3594e16f88e9c9475a9067315b145c
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-05 20:54:30 -0600

    Cleaned up json by changing MovieGenre from a slice to a map.

--- models/models.go
+++ models/models.go
@@ -19,17 +19,17 @@ func NewModels(db *sql.DB) Models {
 
 // Movie is the type for movies
 type Movie struct {
-	ID          int          `json:"id"`
-	Title       string       `json:"title"`
-	Description string       `json:"description"`
-	Year        int          `json:"year"`
-	ReleaseDate time.Time    `json:"release_date"`
-	Runtime     int          `json:"runtime"`
-	Rating      int          `json:"rating"`
-	MPAARating  string       `json:"mpaa_rating"`
-	CreatedAt   time.Time    `json:"_"`
-	UpdatedAt   time.Time    `json:"_"`
-	MovieGenre  []MovieGenre `json:"genres"`
+	ID          int            `json:"id"`
+	Title       string         `json:"title"`
+	Description string         `json:"description"`
+	Year        int            `json:"year"`
+	ReleaseDate time.Time      `json:"release_date"`
+	Runtime     int            `json:"runtime"`
+	Rating      int            `json:"rating"`
+	MPAARating  string         `json:"mpaa_rating"`
+	CreatedAt   time.Time      `json:"_"`
+	UpdatedAt   time.Time      `json:"_"`
+	MovieGenre  map[int]string `json:"genres"`
 }
 
 // Genre is the type for genre
