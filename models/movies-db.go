1855fcb2b78c5731c388221f3688f0421cf089b5
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-05 20:05:50 -0600

    Added descriptive comments to movies-db.go.

--- models/movies-db.go
+++ models/movies-db.go
@@ -6,10 +6,12 @@ type DBModel struct {
 	DB *sql.DB
 }
 
+// Get returns one movie and error, if any
 func (m *DBModel) Get(id int) (*Movie, error) {
 	return nil, nil
 }
 
+// All returns all movies and error, if any
 func (m *DBModel) All(id int) ([]*Movie, error) {
 	return nil, nil
 }
